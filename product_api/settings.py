969300e2595d8f2e0f193b7efe4d637e16457201
Andrew Witmer <witmer.a@gmail.com>
2023-03-28 14:48:35 -0700

    Completed Troubleshooting of Migration to SQL Error

--- product_api/settings.py
+++ product_api/settings.py
@@ -110,3 +110,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from product_api.local_settings import *
+except ImportError:
+    pass
